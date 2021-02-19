cfee661bd1bc6f17130d09018191e31eadcb9b12
Rohit Gund <rohitgund@gmail.com>
2021-02-19 12:44:16 -0800

    Update TwoSum.py

--- AlgoExpert/TwoSum.py
+++ AlgoExpert/TwoSum.py
@@ -1,1 +1,12 @@
 #Two Sum
+def twoNumberSum(array, targetSum):
+	map = {}
+	for num in array:
+		potentialMatch = targetSum - num
+		if potentialMatch in map:
+			return [potentialMatch, num]
+		else:
+			map[num] = True
+	return []
+
+#Time and Space O(n)
